0f260dc2bbf77d040c8469d9b112980441ee1bfe
whycardboard <49077192+whycardboard@users.noreply.github.com>
2021-05-11 21:06:43 -0600

    added way to check for bombs
    kaboom.

--- minesweeper/src/Board.js
+++ minesweeper/src/Board.js
@@ -12,10 +12,39 @@ class Board extends React.Component {
 
     createBoard() {
         let result = [];
+        result.push(<button onClick="isSquareBomb(0, 0)"></button>)
         for(let i = 0; i < 8; i++) {
             result.push(<Row id={i.toString()} key={i.toString()}/>)
         }
         return result;
+    }
+
+    isSquareBomb(x, y) {
+        let rows = document.getElementsByClassName("row");
+    
+        let foundRow;
+        for(let row of rows) {
+            if(row.props.key === y.toString()) {
+                foundRow = row;
+                break;
+            }
+        }
+    
+        let squares = foundRow.getElementsByClassName("square");
+    
+        let foundSquare;
+        for(let square of squares) {
+            if(square.props.key === x.toString()) {
+                foundSquare = square;
+                break;
+            }
+        }
+    
+        if (foundSquare.className.includes("true")) {
+            return true; // BOOM!
+        }
+    
+        return false; // dud.
     }
 
     render() {
